0608938e2376e5853bf04ec49699516b0751d00c
LotusterHaar <lotus.ter.haar@gmail.com>
2022-08-28 09:23:16 +0200

    Setup fx application. Uses FXML loader for layout.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module todu.projects.fractalcreator {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens todu.projects.fractalcreator to javafx.fxml;
+    exports todu.projects.fractalcreator;
+}
---
+++ src/main/java/todu/projects/fractalcreator/Main.java
@@ -0,0 +1,11 @@
+package todu.projects.fractalcreator;
+
+import javafx.application.Application;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Application.launch(HelloApplication.class, args);
+    }
+}
